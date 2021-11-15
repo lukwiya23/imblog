f3bdae0f064a4c7d28e5d217bef2083054bd53b5
lukwiya23 <luk23bonnie8@gmail.com>
2021-11-15 14:16:48 +0300

    postcard complete

---
+++ services/index.js
@@ -0,0 +1,40 @@
+import {request, gql} from 'graphql-request'
+
+
+const graphqlAPI = process.env.GRAPHCMS_ENDPOINT
+export const getPosts = async()=> {
+
+    const query = gql`
+query MyQuery {
+  postsConnection {
+    edges {
+      node {
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        author {
+          bio
+          id
+          name
+          photo {
+            url
+          }
+        }
+        categories {
+          name
+          slug
+        }
+      }
+    }
+  }
+}
+    `
+
+const result = await request(graphqlAPI, query)
+
+return result.postsConnection.edges
+}
